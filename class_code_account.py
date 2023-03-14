10cfbfb1533c0daa80edd8809c6719c924c244c6
Petch Tanathip <65010443@kmitl.ac.th>
2023-03-15 01:54:46 +0700

    commit class code

---
+++ class_code_account.py
@@ -0,0 +1,17 @@
+class Account:
+    def __init__(self, name, surname, email, status):
+        self.__name = name
+        self.__surname = surname
+        self.__email = email
+        self._status = status
+
+class Admin(Account):
+    def __init__(self, permission):
+        Account.__init__(self, name, surname, email, status)
+        self.permission = permission
+
+class User(Account):
+    def __init__(self, country, mobile):
+        Account.__init__(self, name, surname, email, status)
+        self.country = country
+        self.mobile = mobile
