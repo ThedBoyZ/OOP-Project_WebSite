44e960b0a622c3fa6698e7c0f73d6176b0903c20
PetchTanathip <105107167+htc3p@users.noreply.github.com>
2023-03-15 02:51:22 +0700

    Update class_code_account.py

--- class_code_account.py
+++ class_code_account.py
@@ -3,16 +3,17 @@ class Account:
         self.__name = name
         self.__surname = surname
         self.__email = email
-        self._password = password
-        self._status = status
+        self.__password = password
+        self.__status = status
 
 class Admin(Account):
     def __init__(self, permission):
         Account.__init__(self, name, surname, email, password, status)
-        self.permission = permission
+        self.__permission = permission
 
 class User(Account):
-    def __init__(self, country, mobile):
+    def __init__(self, country, mobile, user_id):
         Account.__init__(self, name, surname, email, password, status)
-        self.country = country
-        self.mobile = mobile
+        self.__country = country
+        self.__mobile = mobile
+        self.__user_id = user_id
