c1d83f5717d45e04ba810b72c772ee3b8837d002
Naphat Phatinawin <mick.naphatt@gmail.com>
2023-03-19 23:56:51 +0700

    add class Coupon

---
+++ Coupon.py
@@ -0,0 +1,23 @@
+class Coupon:
+    def __init__(self, code, discount, description, promo_period, travel_period):
+        self.code = code
+        self.discount = discount
+        self.description = description
+        self.promo_period = promo_period
+        self.travel_period = travel_period
+    
+    def descript_coupon_detail():
+        pass
+
+    def update_payment_coupon():
+        pass
+
+class CouponCollection:
+    def __init__(self, coupon_detail):
+        self._coupon_detail = coupon_detail
+
+    def coupon_home():
+        pass
+
+    def get_available_coupon():
+        pass
