ddb37d72363b4e8b3fd933222a4297edbdf75bd3
Petch Tanathip <nm3207petchza@gmail.com>
2023-04-04 15:00:45 +0700

    edit flight

--- flight.py
+++ flight.py
@@ -1,32 +1,24 @@
-from airport_and_airline import AirlineCollection, AirportCollection, Image
+from add_airline import AirlineCollection
+from add_airport import AirportCollection
 from datetime import datetime
 
-airport_coll = AirportCollection()
-airport_coll.add_airport("Suvarnabhumi Airport", "BKK", "")
-airport_coll.add_airport("Don Mueang International Airport", "DMK", "Terminal 2")
-airport_coll.add_airport("Chiang Mai International Airport", "CNX", "Terminal Domestic")
-
-airline_coll = AirlineCollection()
-airline_coll.add_airline("Thai Vietjet Air" , "images/airlines/thai_vietjet_air.png")
-airline_coll.add_airline("Bangkok Airways"  , "images/airlines/bangkok_airways.png")
-airline_coll.add_airline("Thai Smile Air"   , "images/airlines/thai_smile_air.png")
-airline_coll.add_airline("Thai AirAsia"     , "images/airlines/thai_airasia.png")
-airline_coll.add_airline("Nok Air"          , "images/airlines/nok_air.png")
 
 class Flight:
-    def __init__(self, flight_id, departure_airport_code, arrival_airport_code, day, departure_datetime, arrival_datetime, airline_name, cabin_baggage=0, refund=False, reschedule=False, status="On Time"):
+
+    airline_coll = AirlineCollection()
+    airport_coll = AirportCollection()
+
+    def __init__(self, flight_id, departure_airport, arrival_airport, day, departure_datetime, arrival_datetime, airline_name, cabin_baggage=0, refund=False, reschedule=False, status="On Time"):
         self.flight_id = flight_id
         self.cabin_baggage = cabin_baggage
         self.refund = refund
         self.reschedule = reschedule
         self.status = status
-        self.departure_airport_code = departure_airport_code
-        self.arrival_airport_code = arrival_airport_code
+        self.departure_airport = Flight.airport_coll.airports[departure_airport].name
+        self.arrival_airport = Flight.airport_coll.airports[arrival_airport].name
         self.day = day
-        self.airline_name = airline_name
-        self.airline_image = airline_coll.get_airline_image(airline_name)
-        self.departure_airport = airport_coll.get_airport_name(departure_airport_code)
-        self.arrival_airport = airport_coll.get_airport_name(arrival_airport_code)
+        self.airline_name = Flight.airline_coll.airlines[airline_name].name
+        self.airline_logo = Flight.airline_coll.airlines[airline_name].logo
 
         self.departure_datetime = datetime.strptime(departure_datetime, "%Y-%m-%d %H:%M")
         self.departure_year = self.departure_datetime.strftime("%Y")
@@ -42,7 +34,7 @@ class Flight:
 
     
     def __str__(self):
-        return "{!r} Flight {}: {!r} to {!r}".format(self.airline_name, self.flight_id, self.departure_airport, self.arrival_airport)
+        return "Airline: {!r} \nFlight ID: {} \nFrom {!r} ({}) to {!r} ({})".format(self.airline_name, self.flight_id, self.departure_airport, self.departure_datetime, self.arrival_airport, self.arrival_datetime)
     
     @staticmethod
     def get_month(datetime):
@@ -66,30 +58,29 @@ class Flight:
     
 
 class Trip:
-    def __init__(self, flights, count_transit=0):
-        self.flights = flights
-        self.count_transit = count_transit
+    def __init__(self):
+        self.__flights = {}
+        self.__transit = count_transit
+
+    def add_flight(self, flight):
+        if flight.flight_id in self.__flights.values():
+            raise ValueError("Flight already exist")
+        self.__flights[flight_id] = flight
 
     def __str__(self):
         output = "List of Trip:\n"
         for flight in self.flights:
-            output += "{}\n".format(flight)
-        output += "{} transit".format(self.count_transit)
+            output += f"{flight}\n"
+            output += "-"*40
+            output += "\n"
+        output += f"{self.count_transit} transit"
         return output
 
 
 if __name__ == "__main__":
     
-    flight1 = Flight("VZ100", "BKK", "CNX", "Monday", "2023-03-27 10:00", "2023-03-27 18:00", "Thai Vietjet Air")
-    # print(flight1.departure_year)
-    # print(flight1.departure_month)
-    # print(flight1.departure_day)
-    # print(flight1.departure_time)
-    # print(flight1)
+    flight1 = Flight("VZ100", "BKK", "CNX", "Monday", "2023-03-27 10:00", "2023-03-27 12:30", "Thai Vietjet Air")
     flight2 = Flight("FD3416", "DMK", "CNX", "Tuesday", "2023-03-30 05:10", "2023-03-30 06:20", "Thai AirAsia", cabin_baggage=7, refund=False, reschedule=True)
-    # print(flight2)
-    # print(flight2.reschedule)
     
-
     trip1 = Trip([flight1, flight2], count_transit=1)
     print(trip1)
