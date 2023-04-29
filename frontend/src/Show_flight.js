628760d146fc5877ce1b6f596d89ee3b2294f617
Petch Tanathip <nm3207petchza@gmail.com>
2023-04-30 03:59:32 +0700

    edit file name

--- frontend/src/Show_flight.js
+++ frontend/src/Show_flight.js
@@ -1,6 +1,6 @@
 import {React, useState} from "react";
 import axios from "axios";
-import { respond_data as respond } from "./homepage";
+import { respond_data as respond } from "./Homepage";
 
 //console.log(respond_data["status"])
 
