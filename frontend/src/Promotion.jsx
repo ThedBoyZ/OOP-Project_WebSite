bf957025731c129df376d5ed9a4a07cd336a3aaf
Petch Tanathip <nm3207petchza@gmail.com>
2023-04-30 03:38:37 +0700

    promotion

---
+++ frontend/src/Promotion.jsx
@@ -0,0 +1,17 @@
+import { useNavigate } from "react-router-dom";
+import { useState,React } from "react";
+
+export default function Promotion() {
+
+    const show_element = () => {
+        var display = 0
+    }
+
+    return (
+      <>
+        <h1>Promotion page</h1>
+        <input type="text" />
+
+      </>
+    );
+  }
