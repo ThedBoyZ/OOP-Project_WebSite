380aa16ceec66312964062a167136896a6837d65
Naphat Phatinawin <mick.naphatt@gmail.com>
2023-05-02 00:27:40 +0700

    check orders

--- Check_orders_api.py
+++ Check_orders_api.py
@@ -1,7 +1,9 @@
 from typing import Union
 from fastapi import FastAPI
 from Check_orders_for_customer import booking_list
-from Check_orders_for_customer import seat_price_list
+from SeatPrice import seat_price_list
+from SeatPrice import SeatPrice
+
  
 
 app = FastAPI()
@@ -14,9 +16,14 @@ async def root() -> dict:
 async def get_booking() -> object:
     return {"Data": booking_list}
 
-# @app.put("/apply_coupon", tags=['Coupon'])
-# async def apply_coupon(order_code, coupon: str):
-#     order_index = booking_list.check_order_code(order_code)
-#     for seat_price in seat_price_list:
-#         if order_code == seat_price.__order_code:
-#             seat_price.__coupon = coupon
+@app.get("/total_price", tags=['Booking'])
+async def get_total_price(order_code:str):
+    for seat_price in seat_price_list.seat_price_list:
+        if seat_price.order_code == order_code:
+            return seat_price.total_price
+
+@app.put("/apply_coupon", tags=['Coupon'])
+async def apply_coupon(order_code: str, coupon: str):
+    for seat_price in seat_price_list.seat_price_list:
+        if order_code == seat_price.order_code:
+            return seat_price.apply_new_coupon(coupon)
