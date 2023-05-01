8c6a21b082ca6ea07790f4bba19560b14971bc1e
Petch Tanathip <nm3207petchza@gmail.com>
2023-05-02 03:40:57 +0700

    booking api | done

--- backend/app_api.py
+++ backend/app_api.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from booking import *
 from total_price import *
@@ -39,7 +39,7 @@ async def search_flight(depart, arrival, travelday):
 
 @app.post("/booking", tags=["booking"])
 async def add_booking(data: dict):
-    trip = my_trip[2]
+    trip = data['trip_detail']
     contact = Contact(data['contact_name'], data['contact_surname'], data['contact_title'], data['contact_email'], data['contact_mobile'])
     travelers = []
     
@@ -56,3 +56,17 @@ async def add_booking(data: dict):
 async def get_booking(airpaz_code: str):
     booking_details = booking_system.get_booking_by_id(airpaz_code)
     return {"booking_details": booking_details}
+
+# ---------------------------------- Total price ----------------------------------------
+
+@app.post("/discount", tags=["discount"])
+async def discount(data: dict):
+    travelers = []
+    for i in range(int(data['number_of_traveler'])):
+        traveler_data = data['travelers'][i]
+        traveler = Traveler(traveler_data['type_person'], traveler_data['title'], traveler_data['gender'], traveler_data['name'], traveler_data['surname'], traveler_data['dob'], traveler_data['nationality'], traveler_data['baggage_weight'])
+        travelers.append(traveler)
+
+    details = PriceDetailCollection(my_trip[2], travelers)
+    details.discount(data['promo_code'])
+    return {"Update_price_details": details.get_price_details()}
