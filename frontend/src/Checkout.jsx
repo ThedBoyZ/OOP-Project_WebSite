9cda14d96812d766ebd7d45ee869829b7a382671
ping <konpah3019@gmail.com>
2023-05-01 22:44:41 +0700

    checkout

--- frontend/src/Checkout.jsx
+++ frontend/src/Checkout.jsx
@@ -5,34 +5,35 @@ import { selected_value } from "./show_flight";
 
 export default function Checkout() {
   const navigate = useNavigate();
-  const [airline, setAirline] = useState("");
-
-  const test = () =>{
+  const [jsonItems, setJsonItems] = useState([]);
+  
+  const show_select = () =>{
     console.log(selected_value['data'])
+    const data = []
+    for(let i in selected_value['data']){
+      data.push(selected_value['data'][i])
+    }
+   
+    const listItems = data.map((d, index) =>
+    <ul key={index}>
+      {
+        <h5>{d}</h5>
+      }
+    </ul>);
+    return listItems
+  }
+
+  const checkout = () =>{
+    //navigate('./payment')
+    console.log('payment')
   }
   
 
-  const checkout = (event) => {
-    event.preventDefault(); // prevent form submission behavior
-    
-    axios.post("http://127.0.0.1:8000/add_flight", {
-      
-        
-    
-    })
-    .then(res => {
-      console.log(res.data);
-      
-    })
-    
-  }
+  
   return (
     <>
-      <h1>Checkout</h1>
-      {test()}
-        
-      
-        
+      {show_select()}
+      <button type="button" onClick={() => checkout()}>CHECKOUT</button>
     </>
   );
 }
