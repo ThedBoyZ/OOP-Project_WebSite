2d80f93c7a2a59816ccac885b711514bd1169919
Petch Tanathip <nm3207petchza@gmail.com>
2023-05-01 22:46:31 +0700

    calculate price | done

--- backend/total_price.py
+++ backend/total_price.py
@@ -1,23 +1,79 @@
 from datetime import date
 from account import Traveler
+from flight import Trip
+from Coupon import coupon_list
 
 
 # Define a class to hold price details for a single passenger
 class PriceDetail:
-    def __init__(self, type_person, baggage_weight):
-        self._init_person_price(type_person)
+
+    DISTANCE_INDEX_MAP = {
+        ("BKK", "HDY"): 5,
+        ("HDY", "BKK"): 5,
+        ("BKK", "HKT"): 4,
+        ("HKT", "BKK"): 4,
+        ("BKK", "CNX"): 3,
+        ("CNX", "BKK"): 3,
+        ("BKK", "UBP"): 2,
+        ("UBP", "BKK"): 2,
+        ("BKK", "UTH"): 1,
+        ("UTH", "BKK"): 1,
+    }
+
+    def __init__(self, day, departure, arrival, airline_name, person_type, baggage_weight):
+        self.day = day
+        self.departure = departure
+        self.arrival = arrival
+        self.airline_name = airline_name
+        self.person_type = person_type
+        self.distance_index = self.calculate_distance_index()
         self._init_baggage_price(baggage_weight)
 
-    def _init_person_price(self, person_type: str):
-        # Initialize person price based on type
-        if person_type.upper() == "ADULT":
-            self.person_price = 800
-        elif person_type.upper() == "CHILD":
-            self.person_price = 800
-        elif person_type.upper() == "INFANT":
-            self.person_price = 300
+    def calculate_distance_index(self) -> int:
+        key = (self.departure, self.arrival)
+        if key in PriceDetail.DISTANCE_INDEX_MAP:
+            return PriceDetail.DISTANCE_INDEX_MAP[key]
+        else:
+            raise ValueError(f"Invalid departure/arrival airports: {self.departure}/{self.arrival}")
+
+    def calculate_person_price(self) -> float:
+        if self.airline_name == "Thai Vietjet Air":
+            if self.day in ["Saturday", "Sunday"]:
+                base_price = 1200
+            else:
+                base_price = 900
+            price_per_distance = 49
+        elif self.airline_name == "Bangkok Airways":
+            if self.day in ["Saturday", "Sunday"]:
+                base_price = 1200
+            else:
+                base_price = 900
+            price_per_distance = 52
+        elif self.airline_name == "Thai Smile Air":
+            if self.day in ["Saturday", "Sunday"]:
+                base_price = 1200
+            else:
+                base_price = 900
+            price_per_distance = 54
+        elif self.airline_name == "Thai AirAsia":
+            if self.day in ["Saturday", "Sunday"]:
+                base_price = 1200
+            else:
+                base_price = 900
+            price_per_distance = 56
+        else:
+            raise ValueError("Invalid airline name.")
+
+        if self.person_type.upper() == "ADULT":
+            person_price = base_price + (self.distance_index * price_per_distance)
+        elif self.person_type.upper() == "CHILD":
+            person_price = base_price + (self.distance_index * price_per_distance)
+        elif self.person_type.upper() == "INFANT":
+            person_price = 300
         else:
             raise ValueError("Invalid person type.")
+
+        return person_price
 
     def _init_baggage_price(self, baggage_weight):
         # Initialize baggage price based on weight
@@ -37,32 +93,75 @@ class PriceDetail:
 
     def total_price(self) -> int:
         # Return the sum of the person and baggage prices
-        return self.person_price + self.baggage_price
+        return self.calculate_person_price() + self.baggage_price
 
 
 # Define a class to hold a collection of price details for multiple passengers
 class PriceDetailCollection:
-    def __init__(self):
-        self.details = []
+    def __init__(self, trip):
+        self.number_of_adult = 0
+        self.number_of_child = 0
+        self.number_of_infant = 0
+        self.__trip = trip
+        self.__details = []
 
     def add_price_detail(self, traveler: Traveler):
-        detail = PriceDetail(traveler.type_person, traveler.baggage_weight)
-        self.details.append(detail)
+        detail = PriceDetail(self.__trip["day"], self.__trip["departure_airport"], self.__trip["arrival_airport"], self.__trip["airline_name"], traveler.type_person, traveler.baggage_weight)
+        self.__details.append(detail)
+        if detail.person_type == "Adult":
+            self.number_of_adult += 1
+        elif detail.person_type == "Child":
+            self.number_of_child += 1
+        elif detail.person_type == "Infant":
+            self.number_of_infant += 1
+
+    def discount(self, promo_code = '0'):
+        for coupon in coupon_list._coupon_detail:
+            if promo_code == coupon.code:
+                return self.total_price() * (100 - coupon.discount) / 100
 
     def total_price(self) -> int:
         # Return the sum of the total prices for all price details in the collection
-        return sum(detail.total_price() for detail in self.details)
+        return sum(detail.total_price() for detail in self.__details)
 
+    def get_price_details(self):
+        output = {}
+
+        for detail in self.__details:
+            price_detail = detail
+            if price_detail.person_type == "Adult":
+                output["Adult"] = {
+                    "Number_of_Adult": self.number_of_adult,
+                    "Total": price_detail.calculate_person_price()
+                }
+            if price_detail.person_type == "Child":
+                output["Child"] = {
+                    "Number_of_Child": self.number_of_child,
+                    "Total": price_detail.calculate_person_price()
+                }
+            if price_detail.person_type == "Infant":
+                output["Infant"] = {
+                    "Number_of_Infant": self.number_of_infant,
+                    "Total": price_detail.calculate_person_price()
+                }
+        
+        output["Total_price"] = self.total_price()
+        return output
 
 if __name__ == '__main__':
 
     t1 = Traveler("Child", "", "Male", "Peter", "Parker", date(2007, 10, 14), "Thailand")
-    t2 = Traveler("Adult", "Mr.", "Female", "Pim", "Niyom", date(1999, 3, 7), "Thailand")
+    t2 = Traveler("Adult", "Mr.", "Female", "Pim", "Niyom", date(1999, 3, 7), "Thailand", "15")
     t3 = Traveler("Infant", "", "Male", "Angry", "Bird", date(2023, 3, 27), "Thailand")
 
-    details = PriceDetailCollection()
+    trip1 = Trip()
+    my_trip = trip1.search_flight('BKK', 'CNX', 'Friday')
+
+    details = PriceDetailCollection(my_trip[2])
     details.add_price_detail(t1)
     details.add_price_detail(t2)
     details.add_price_detail(t3)
 
-    print("Total price:", details.total_price(), "bath")
+    # print("Total price:", details.total_price(), "bath")
+    # print(details.get_price_details())
+    print(details.discount("dc10"))
